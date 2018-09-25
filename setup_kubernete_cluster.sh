d7157529c4e0aee51294224acd270b4f29365462
shi <swt914@aliyun.com>
2018-09-25 16:08:17 +0800

    add kubernete script

---
+++ setup_kubernete_cluster.sh
@@ -0,0 +1,125 @@
+
+install_kubetool(){
+apt-get update && apt-get install -y apt-transport-https
+curl  http://mirrors.cloud.aliyuncs.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://mirrors.cloud.aliyuncs.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+}
+
+#masterip=`ifconfig eth0 | grep "inet addr:" | awk '{print $2}' | awk -F ":" '{print $2}'`
+masterip=172.17.253.187
+echo $masterip
+docker_mirror_server="$masterip:5000"
+
+k8s_master_ip=`ifconfig eth0 | grep "inet addr:" | awk '{print $2}' | awk -F ":" '{print $2}'`
+
+set_docker_damon(){
+
+apt update && apt-get install -y docker.io
+
+cat <<EOF >/etc/docker/daemon.json
+{"insecure-registries":["$docker_mirror_server"]}
+EOF
+
+systemctl restart docker.service
+
+
+}
+
+setup_kubelet_conf(){
+sed -i "3i\Environment='KUBELET_POD_INFRA_ARGS=--pod-infra-container-image=$docker_mirror_server/pause:3.1'" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+sed -i 's#$KUBELET_KUBECONFIG_ARGS#$KUBELET_KUBECONFIG_ARGS $KUBELET_POD_INFRA_ARGS#g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+
+
+}
+setup_registry_server(){
+set_docker_damon
+
+dockerdata=$HOME/registry-data
+mkdir $dockerdata -p
+docker run -d -p 5000:5000 -v $dockerdata:/var/lib/registry registry:2
+
+docker login -u shiwt -p shiwt
+
+docker pull shiwt/kube-proxy-amd64 -a && docker tag shiwt/kube-proxy-amd64:v1.11.3 $docker_mirror_server/kube-proxy-amd64:v1.11.3 && docker push $docker_mirror_server/kube-proxy-amd64:v1.11.3
+docker pull shiwt/kube-apiserver-amd64 -a && docker tag shiwt/kube-apiserver-amd64:v1.11.3 $docker_mirror_server/kube-apiserver-amd64:v1.11.3 && docker push $docker_mirror_server/kube-apiserver-amd64:v1.11.3
+docker pull shiwt/kube-controller-manager-amd64 -a && docker tag shiwt/kube-controller-manager-amd64:v1.11.3 $docker_mirror_server/kube-controller-manager-amd64:v1.11.3 && docker push $docker_mirror_server/kube-controller-manager-amd64:v1.11.3
+docker pull shiwt/kube-scheduler-amd64 -a && docker tag shiwt/kube-scheduler-amd64:v1.11.3 $docker_mirror_server/kube-scheduler-amd64:v1.11.3 && docker push $docker_mirror_server/kube-scheduler-amd64:v1.11.3
+docker pull shiwt/coredns -a && docker tag shiwt/coredns:1.1.3  $docker_mirror_server/coredns:1.1.3 && docker push $docker_mirror_server/coredns:1.1.3
+docker pull shiwt/etcd-amd64 -a && docker tag shiwt/etcd-amd64:3.2.18 $docker_mirror_server/etcd-amd64:3.2.18 && docker push $docker_mirror_server/etcd-amd64:3.2.18
+docker pull shiwt/pause -a && docker tag shiwt/pause:3.1  $docker_mirror_server/pause:3.1 && docker push $docker_mirror_server/pause:3.1
+
+newregistry_container_id=`docker ps | grep "registry:2" | awk '{print $1}'`
+docker commit -m "save newregistry" $newregistry_container_id $docker_mirror_server/registry:2
+docker push $docker_mirror_server/registry:2
+
+
+docker pull $docker_mirror_server/pause:3.1
+
+curl -XGET $docker_mirror_server/v2/_catalog
+
+du -slh $dockerdata/
+docker logout
+}
+
+
+install_kubecluster(){
+
+cat <<EOF > $HOME/kubeadm.yaml
+apiVersion: kubeadm.k8s.io/v1alpha1
+kind: MasterConfiguration
+api:
+  advertiseAddress: "$k8s_master_ip"
+networking:
+  podSubnet: "10.244.0.0/16"
+kubernetesVersion: "v1.11.3"
+imageRepository: "$docker_mirror_server"
+
+EOF
+
+setup_kubelet_conf
+
+kubeadm init --config $HOME/kubeadm.yaml
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+sysctl net.bridge.bridge-nf-call-iptables=1
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
+kubectl apply -f $HOME/kube-flannel.yml
+
+
+kubectl get node
+}
+
+
+echo "iput your answer"
+if [ $1 == "install_registry" ];then
+    echo "install registry"
+    install_registry
+fi
+
+if [ $1 == "install_kubetool" ];then
+echo "install kubetool"
+install_kubetool
+fi
+
+if [ $1 == "install_kubecluster" ];then
+
+echo "install_kubecluster"
+install_kubecluster
+
+fi
+
+
+if [ $1 == "set_docker_damon" ];then
+   set_docker_damon
+fi
+
+
